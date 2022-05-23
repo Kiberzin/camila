14b8995e69d5d94ff86f22e92b787bb2e7e2040b
Gabriel Mendonça <mendoncagabriel753@gmail.com>
2022-05-23 11:43:05 -0300

    first commit

---
+++ src/anel/anel1.java
@@ -0,0 +1,33 @@
+package anel;
+
+import java.util.Scanner;
+
+public class anel1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		Scanner ler = new Scanner (System.in);
+		
+		int oi , io , oiio, re;
+		System.out.println("Olá Mundo Cruel");
+		oi = ler.nextInt();
+		
+		System.out.println("Olá Mundo Cruel");
+		io = ler.nextInt();
+		
+		System.out.println("Olá Mundo Cruel");
+		oiio = ler.nextInt();
+		
+		
+		re = oi + io + oiio;
+		
+		System.out.println("Seu resultado é " + re);
+		
+		
+		
+		
+	}
+
+}
